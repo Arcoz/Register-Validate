56db97a108ffa5acc41daedee6131509ddf5ddf3
Dyrhoi <arcoz@stofanet.dk>
2015-03-16 16:28:05 +0100

    linkchange

--- registration.php
+++ registration.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 
 $registration_id = rand(1000000, 2400000000);
 
-$message = '<a href="http://dyrhoi.com/test_registration/register.php?id='.$registration_id.'">Click here</a>';
+$message = '<a href="http://.../register.php?id='.$registration_id.'">Click here</a>';
 
 $sql = "INSERT INTO users (users_email, users_secureid) VALUES ('$email', '$registration_id')";
 mysqli_query($mysqli, $sql);
